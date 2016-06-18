4aab0f995c60275ecd3602d90d9c9825c4458188
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2016-06-18 09:31:02 -0600

    basic html and css layout

---
+++ scripts.js
@@ -0,0 +1,10 @@
+
+var player1 = "x";
+var player2 = "o";
+var boardCells = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+
+
+//listen for click
+//document.addEventListener("click", //addfunction that goes into play when click happens
