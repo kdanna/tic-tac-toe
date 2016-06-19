6ed3335dd951b93912c7c66c2b2d66a93271802e
YOUR-USERNAME <YOUR-EMAIL-ADDRESS>
2016-06-19 15:05:20 -0600

    X's and O's appear. Game will not allow the same cell to be clicked twice. Cannot figure out how to determine winner or clear board.  My for those functions are commented out.

--- scripts.js
+++ scripts.js
@@ -2,41 +2,44 @@
 	//figure out ways to put the event listen in the DOM
 	//figure out how to avoid changing a cell once it has been used
 
-
-
 	var turn = "x";
-	var theBoxes = document.getElementsByClassName("cell");
-	
+	var theBoxes = document.getElementsByClassName("cell"); //returns each cell
+	var clear = document.getElementsByClassName("playAgain");
+
 	//TESTED. the start function happens on window load and sets 'x' to begin.
 	function startGame() {
 		theBoxes.innerText = turn;
-
+		winner = null;
 	}
 
 	//TESTED. the nextPlayer functions checks to see what the value of turn is and assigns the opposite.
 	function nextPlayer(){
+		/*if (isWinner() === true){
+			alert("we have a winner");*/
 		if (turn === "x"){
 			turn = "o";
 		}else {
 			turn = "x";
-		}; 
-	};
-
-
+	}
+};
 
 	// TESTED. the nextMove function is triggered by a click on the cell (from the html tag)
 	function nextMove(cell) {	
-		if(theBoxes.innerText = " "){
-		 	cell.innerText = turn;
+		if(cell.innerText === ""){
+			cell.innerText = turn;
 		 	nextPlayer();
+		}else {
+			alert("That box is already taken. Try again.");
 		}
 	}
 
 	
+/*Below are all my ideas for 1) determining winner and 2) making play again button fuction.  After two days of working on this non-stop I threw in the towel.
 
-	//I think i might have to set this as an array since that is how the divs were identified by query search
+
+	//NOT WORKING 
 	function isWinner(move){
-		var gameOver = false;
+		var winner = false;
 		if  (threeInaRow(1,2,3, move)||
 			threeInaRow(4,5,6, move) ||
 			threeInaRow(7,8,9, move) ||
@@ -44,26 +47,33 @@
 			threeInaRow(3,5,7, move) ||
 			threeInaRow(1,4,7, move) ||
 			threeInaRow(2,5,8, move) ||
-			threeInaRow(3,6,9, move)){
-		
-			gameOver = true;
+			threeInaRow(3,6,9, move)) {
+			winner = true;
 			}
-		return gameOver;
+		return winner;
 		};
 
+ 	// should return the "x" or "o" and does when you test it solo
+	function getBoxStatus(number){
+		return theBoxes.innerText;
+	}
 
-	// trying to determine what 
+	// This function works on its own when you pass is all "x" or "o", but it is returning false when used with other fucntions
+
 	function threeInaRow(x,y,z, move){
 		var results = false;
-		if (status(x) === move && status(y) === move && status(z) === move){
-		var results = true;
+		if (getBoxStatus(x) === move && getBoxStatus(y) === move && getBoxStatus(z) === move){
+		 results = true;
 		}
 		return results;
 	}
 
 
-	function getBoxStatus(number){
-		var status = document.querySelectorAll('div div');
 
-	}
+General Idea for removing text from game, ie "resetting the board"
+function eraseText(){
+	document.getElementsByClassName("cell").empty();
+}
+*/
+
 
---
+++ scripts2.js
@@ -0,0 +1,3 @@
+//trying out different ideas on this js.
+
+var eachSquare = document.querySelectorAll('div div');
